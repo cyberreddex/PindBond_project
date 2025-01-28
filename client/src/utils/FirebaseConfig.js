4fc9a4ac196402841ec776570aaf7fd0b63bf909
Adesh Kumar Yadav <141759480+cyberreddex@users.noreply.github.com>
2025-01-28 17:20:56 +0530

    Update FirebaseConfig.js

--- client/src/utils/FirebaseConfig.js
+++ client/src/utils/FirebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDnKsrF37GbvSyUGM2YjRljeRgz-5PF3B4",
-    authDomain: "pingbond-3dbf1.firebaseapp.com",
-    projectId: "pingbond-3dbf1",
-    storageBucket: "pingbond-3dbf1.appspot.com",
-    messagingSenderId: "368956106948",
-    appId: "1:368956106948:web:b87b377e46cc2369db4191",
-    measurementId: "G-6B3LR7YC8J"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 
